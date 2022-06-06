c6a1a3d1ad3c1c18ab843fc042337d34f29e9e39
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-06-06 23:06:44 +0600

    added file now

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'https://zara-car-manufacturer-house.web.app/' }));
+
+// middleware
+app.use(cors({ origin: `https://zara-car-manufacturer-house.web.app/` }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.liwrp.mongodb.net/?retryWrites=true&w=majority`;
