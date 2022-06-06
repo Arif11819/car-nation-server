131e1f14aa1bab1abb90da9d0e3f52fce7778790
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-06-06 23:55:06 +0600

    update cors problem

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-// all middleware 
-app.use(cors({ origin: `https://zara-car-manufacturer-house.web.app/` }));
+// middleware 
+app.use(cors({ origin: 'https://zara-car-manufacturer-house.web.app/' }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.liwrp.mongodb.net/?retryWrites=true&w=majority`;
