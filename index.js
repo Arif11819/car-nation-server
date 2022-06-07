fb243430ba631fb444e37a3ff69a4cea8b6a7365
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-06-07 21:48:47 +0600

    added all

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 
 
 // middleware 
-app.use(cors({ origin: 'https://zara-car-manufacturer-house.web.app/' }));
+app.use(cors());
+// app.use(cors({ origin: 'https://zara-car-manufacturer-house.web.app/' }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.liwrp.mongodb.net/?retryWrites=true&w=majority`;
